dfef93c3cff7672c57bf89eca8416ea253848855
SinkevichV <sinkevichvasiliu@mail.ru>
2022-12-19 16:53:59 +0700

    homework2

--- Seminar 2/2_1.py
+++ Seminar 2/2_1.py
@@ -1,1 +1,8 @@
-print("HI!")
+# 1. Напишите программу, которая принимает на 
+# вход число N и выдаёт последовательность из N членов.
+
+def n_func():
+    for i in range(int(input("Input your number, please: "))):
+        print((-3) ** i, end = ' ')
+
+n_func()
